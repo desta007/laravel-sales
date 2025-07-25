f744a2f46de7320bb54e1708a87c36f9d475abca
Desta CP <detajuventini@gmail.com>
2025-07-25 14:32:30 +0700

    modify api

--- app/Filament/Resources/SalesResource/Pages/CreateSales.php
+++ app/Filament/Resources/SalesResource/Pages/CreateSales.php
@@ -5,8 +5,19 @@ namespace App\Filament\Resources\SalesResource\Pages;
 use App\Filament\Resources\SalesResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use App\Models\User;
 
 class CreateSales extends CreateRecord
 {
     protected static string $resource = SalesResource::class;
+
+    protected function afterCreate(): void
+    {
+        $sales = $this->record;
+        User::create([
+            'name' => $sales->name,
+            'email' => $sales->email,
+            'password' => $sales->password, // already hashed
+        ]);
+    }
 }
--- app/Filament/Resources/SalesResource/Pages/EditSales.php
+++ app/Filament/Resources/SalesResource/Pages/EditSales.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\SalesResource\Pages;
 use App\Filament\Resources\SalesResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use App\Models\User;
 
 class EditSales extends EditRecord
 {
@@ -15,5 +16,34 @@ class EditSales extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        $sales = $this->record;
+        $user = User::where('email', $sales->getOriginal('email'))->first();
+        if ($user) {
+            $user->update([
+                'name' => $sales->name,
+                'email' => $sales->email,
+                'password' => $sales->password, // already hashed
+            ]);
+        } else {
+            // If user not found (email changed or missing), create new
+            User::create([
+                'name' => $sales->name,
+                'email' => $sales->email,
+                'password' => $sales->password,
+            ]);
+        }
+    }
+
+    protected function afterDelete(): void
+    {
+        $sales = $this->record;
+        $user = \App\Models\User::where('email', $sales->email)->first();
+        if ($user) {
+            $user->delete();
+        }
     }
 }
